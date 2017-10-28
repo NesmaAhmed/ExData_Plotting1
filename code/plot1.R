79745ce8196ac56ae85946bc41c9a17cd232d990
NesmaAhmed <31442965+NesmaAhmed@users.noreply.github.com>
2017-10-28 13:50:59 +0200

    Update plot1.R

--- code/plot1.R
+++ code/plot1.R
@@ -1,42 +1,11 @@
 library(sqldf)
 subset_data <- read.csv2.sql("F:/data science specialist/course4/data/household_power_consumption.txt", sql = "select * from file where Date == '1/2/2007' or Date == '2/2/2007' ", 
                             na.strings = "?")
-subset_data$Date <- as.Date(subset_data$Date, format = "%d/%m/%Y")              
-Data_Time <- paste(subset_data$Date, subset_data$Time) 
-Data_Time <- strptime(Data_Time, "%Y-%m-%d %H:%M:%S")  # Appropriate time format
+
 ## question 1
 # Create the histogram with data
 png(file = "plot1.png")
 hist(subset_data$Global_active_power, col = "Red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 dev.off()
 
-####q2
-# Correcting the data format
-# Sys.setlocale("LC_ALL","English") to change from arabic to english
-png(file = "plot2.png")
-plot(Data_Time,subset_data$Global_active_power,type = "l",xlab = "",ylab = "Global Active Power (kilowatts)")
-dev.off()
-#### q3
-png(file = "plot3.png")
-plot(Data_Time,subset_data$Sub_metering_1,type="l",col="Black",xlab = "",ylab = "Energy sub metering")
-points(Data_Time,subset_data$Sub_metering_2,type="l",col="Red")
-points(Data_Time,subset_data$Sub_metering_3,type="l",col="Blue")
-legend("topright", lty = 1, col = c("Black","Red","Blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
-dev.off()
-
-###### q4
-png(file = "plot4.png")
-par(mfrow = c(2,2))
-#1
-plot(Data_Time,subset_data$Global_active_power,type = "l",xlab = "",ylab = "Global Active Power (kilowatts)")
-#2
-plot(Data_Time,subset_data$Voltage,type = "l",xlab = "datetime",ylab = "Voltage")
-#3
-plot(Data_Time,subset_data$Sub_metering_1,type="l",col="Black",xlab = "",ylab = "Energy sub metering")
-points(Data_Time,subset_data$Sub_metering_2,type="l",col="Red")
-points(Data_Time,subset_data$Sub_metering_3,type="l",col="Blue")
-legend("topright", lty = 1, col = c("Black","Red","Blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty = "n")
-#4
-plot(Data_Time,subset_data$Global_reactive_power,type = "l",xlab = "datetime",ylab = "Global_reactive_power")
-dev.off()
 sqldf() # close connection
